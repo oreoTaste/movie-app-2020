47c0e361b3a097fc70c01ed03f2284c56c08775a
YoungkukSohn <youngkuk.sohn@gmail.com>
2020-08-11 21:00:13 +0900

    restore for further process

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React from 'react';
 
-// regarding the React.Component
-// 1. Mounting : constructor > render > componentDidMount
-// 2. Updating : reder > componentDidUpdate
-// 3. UnMounting : componentWillUnmount
 class App extends React.Component {
-  // 1. Mounting (constructor) : it is actually from JS
-  constructor(props) {
-    super(props);
-    console.log("constructor")
-  }
-
   state = {
     count: 0
   };
@@ -22,21 +12,7 @@ class App extends React.Component {
     this.setState(current => ({count: current.count - 1}));
   };
 
-  // 3. Unmounting (componentWillUnmount)
-  componentWillUnmount(){
-    console.log("component will unmount")
-  }
-  // 2. Updating (componentDidUpdate)
-  componentDidUpdate() {
-    console.log("component did update")
-  }
-  // 1. Mounting (componentDidMount)
-  componentDidMount(){
-    console.log("conponent rendered")
-  }
-  // 1. Mounting + Updating (render)
   render() {
-    console.log("i'm rendering")
     return (
       <div>
         <h1>The number is {this.state.count}</h1>
