19e13a5835e950f4caddf41d2ca3a54fb56386c7
YoungkukSohn <youngkuk.sohn@gmail.com>
2020-08-12 10:22:16 +0900

    Updates

---
+++ precache-manifest.8ff484831efae56f47473d9c5d25ac51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979a24948f03bffb9720e4f295acdb48",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "e0788de0582f8ba92ab6",
+    "url": "/movie_app_2020/static/css/main.6eec8be2.chunk.css"
+  },
+  {
+    "revision": "22cb90122fd5507e6a66",
+    "url": "/movie_app_2020/static/js/2.ff56ef60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.ff56ef60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0788de0582f8ba92ab6",
+    "url": "/movie_app_2020/static/js/main.423cd871.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
