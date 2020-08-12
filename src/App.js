3080f389e2937241028ddfd301cc8f9142fa921f
YoungkukSohn <youngkuk.sohn@gmail.com>
2020-08-12 10:29:36 +0900

    5.0 Deploying to Github Pages

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
   };
 
   getMovies = async() => {
-    const {data:{data:{movies}}} = await axios.get("http://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
     this.setState({movies, isLoading: false});
   };
 
